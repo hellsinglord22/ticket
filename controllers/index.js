e393d9b613d8a5d12ed2083e8d9359f77a6b5249
Hesham Adel <hesham.adel@pickspace.com>
2024-04-24 22:27:16 +0200

    adds login page and redirections

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,11 @@
 exports.getIndex = (req, res, next) => {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Ticket System' });
+}
+
+exports.unauthorized = (req, res, next) => {
+    res.render('unauthorized');
+}
+
+exports.internalServerError = (req, res, next) => {
+    res.render('server_error');
 }
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var indexController = require('../controllers/index.js');
 /* GET home page. */
 router.get('/', indexController.getIndex);
+router.get('/unauthorized', indexController.unauthorized);
+router.get('/server_error', indexController.internalServerError);
 
 module.exports = router;
